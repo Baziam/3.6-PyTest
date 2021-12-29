eea0c04d67d23185f95185c011532d8b0119d339
Vadim Khalitov <khalitovvyu@holding.melonfashion.ru>
2021-12-29 15:19:30 +0300

    Создал папку Pages с двумя файлами

---
+++ pages/main_page.py
@@ -0,0 +1,7 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+
+class MainPage(BasePage): 
+    def go_to_login_page(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
+        #login_link.click() 
