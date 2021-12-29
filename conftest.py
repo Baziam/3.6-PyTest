1951c52f2bad435d8f11555820451500beb10103
Vadim Khalitov <khalitovvyu@holding.melonfashion.ru>
2021-12-29 15:21:10 +0300

    Обновил conftest

--- conftest.py
+++ conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
+from selenium.webdriver.support.ui import WebDriverWait
 
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
